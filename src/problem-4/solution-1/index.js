1a20b0d1a9bb812b166e83e96373094130656566
Joilson Cisne <joilsoncisne@gmail.com>
2018-12-08 18:52:19 -0200

    Add solution 1 without implementing O(n) sorting

--- src/problem-4/solution-1/index.js
+++ src/problem-4/solution-1/index.js
@@ -1,7 +1,24 @@
 module.exports = (input) => {
-  for (let i = 1;; i++) {
-    if (!input.includes(i)) {
-      return i
+  input.sort() // TODO: Use radix sort to get O(n) complexity
+
+  let candidate = 1
+  
+  for (let i = 0; i < input.length; i++) {
+    let current = input[i];
+
+    if (current <= 0) {
+      continue
+    }
+
+    if (candidate === current) {
+      ++candidate
+      continue
+    }
+
+    if (current > candidate + 1) {
+      break
     }
   }
+
+  return candidate
 }
