cf7fb38d7578e9debdf7691afc473554b1d3926e
Joilson Cisne <joilsoncisne@gmail.com>
2018-12-08 17:31:59 -0200

    Add brute force solutions

---
+++ src/problem-4/solution-1/index.spec.js
@@ -0,0 +1,28 @@
+const { expect } = require('chai')
+const solve = require('.')
+
+describe('Problem #4 - solution 1', () => {
+  describe('When input is [3, 4, -1, 1]', () => {
+    it('should return 2', () => {
+      expect(solve([3, 4, -1, 1])).to.equal(2)
+    });
+  });
+
+  describe('When input is [1, 2, 0]', () => {
+    it('should return 2', () => {
+      expect(solve([1, 2, 0])).to.equal(3)
+    });
+  });
+
+  describe('When input is [-1, -2, -3]', () => {
+    it('should return 2', () => {
+      expect(solve([-1, -2, -3])).to.equal(1)
+    });
+  });
+
+  describe('When input is []', () => {
+    it('should return 1', () => {
+      expect(solve([])).to.equal(1)
+    });
+  });
+});
---
+++ src/problem-4/solution-2/index.js
@@ -0,0 +1,7 @@
+module.exports = (input) => {
+  let i = 0
+
+  while (input.includes(++i)) {}
+
+  return i
+}
